9ba34c41e4fa957ef3a205fe8444370f7efd4dab
JeongUkJae <jeongukjae@gmail.com>
2020-08-19 02:48:20 +0900

    remove unused file

--- build.bat
+++
@@ -1,4 +0,0 @@
-:: this script can only be run under visual studio tools command line
-:: (search "Command Prompt" on start menu to pick one)
-
-devenv.com vsproject\cfbreader\cfbreader.sln /build Debug
